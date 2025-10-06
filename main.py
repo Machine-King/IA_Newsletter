455df8e5a0e43f04171803f05eda26f6860819f5
Machine King <98271849+Machine-King@users.noreply.github.com>
2025-10-06 14:01:19 +0200

    Debug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 
 import os
+import jinja2
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
